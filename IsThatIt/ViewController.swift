b75248dd25b0ebedb06d9592dcec328fe7be8c39
Bogdan <c0pland@icloud.com>
2022-08-23 20:39:14 +0300

    325 - Add CoreML

--- IsThatIt/ViewController.swift
+++ IsThatIt/ViewController.swift
@@ -1,19 +1,52 @@
-//
-//  ViewController.swift
-//  IsThatIt
-//
-//  Created by Богдан Беннер on 22.08.22.
-//
-
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	
+	@IBOutlet weak var imageView: UIImageView!
+	let imagePicker = UIImagePickerController()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		imagePicker.delegate = self
+		imagePicker.sourceType = .camera
+		imagePicker.allowsEditing = false
 	}
-
-
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		if let userImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+			imageView.image = userImage
+			guard let ciImage = CIImage(image: userImage) else {
+				fatalError("Couldn't convert to CIImage")
+			}
+			detect(image: ciImage)
+		}
+		imagePicker.dismiss(animated: true)
+		
+	}
+	
+	func detect(image: CIImage) {
+		guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+			fatalError("Error getting the model")
+		}
+		let request = VNCoreMLRequest(model: model) { request, error in
+			guard let results = request.results as? [VNClassificationObservation] else {
+				fatalError("Cannot convert results to VNClassificationObservation")
+			}
+			print(results)
+		}
+		let handler = VNImageRequestHandler(ciImage: image)
+		do{
+		try handler.perform([request])
+		} catch {
+			print(error)
+		}
+	}
+	
+	@IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+		present(imagePicker, animated: true)
+	}
+	
 }
 
